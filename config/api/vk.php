257a7cc61ebdcffafa2248d708a050d155046f93
Dmitry Bykov <d.bykov@napopravku.ru>
2020-07-12 13:45:45 +0300

    Fixes

--- config/api/vk.php
+++ config/api/vk.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'access_token' => 'fe670e41fe670e41fe670e414cfe15f1a9ffe67fe670e41a16d67ee4a296048ec509f19',
+    'access_token' => env('VK_API_TOKEN'),
     'version' => '5.21',
 ];
